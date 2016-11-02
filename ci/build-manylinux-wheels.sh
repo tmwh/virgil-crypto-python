d870f25a22f11a9550262a9b2a413f54cb0de4ba
Dmitriy Dudkin <dudkin.dmitriy@gmail.com>
2016-11-02 20:53:07 +0200

    Extracted docker images

--- ci/build-manylinux-wheels.sh
+++ ci/build-manylinux-wheels.sh
@@ -2,18 +2,6 @@
 set -e -x
 shopt -s extglob
 
-# TODO: Create docker image with required utilities
-# Install swig using miniconda
-wget --quiet https://repo.continuum.io/miniconda/Miniconda2-4.1.11-Linux-x86_64.sh -O ~/miniconda.sh
-/bin/bash ~/miniconda.sh -b -p /opt/conda
-rm ~/miniconda.sh
-export PATH=/opt/conda/bin:$PATH
-conda install -y swig
-# Install cmake
-wget --no-check-certificate https://cmake.org/files/v3.6/cmake-3.6.2-Linux-x86_64.tar.gz
-tar xzvf cmake-3.6.2-Linux-x86_64.tar.gz
-cp cmake-3.6.2-Linux-x86_64/* /usr/ -rf
-
 # Compile wheels
 for PYBIN in /opt/python/!(cp26*)/bin; do
   ${PYBIN}/pip install -r /io/requirements-dev.txt
