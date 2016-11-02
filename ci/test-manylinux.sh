9ef33fc51331ea86de249bb78a59edc127775716
Dmitriy Dudkin <dudkin.dmitriy@gmail.com>
2016-11-03 00:07:00 +0200

    Fixed travis test script

--- ci/test-manylinux.sh
+++ ci/test-manylinux.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -v `pwd`:/io $DOCKER_IMAGE $PRE_CMD /io/travis/build-manylinux-wheels.sh
+docker run --rm -v `pwd`:/io $DOCKER_IMAGE $PRE_CMD /io/ci/build-manylinux-wheels.sh
